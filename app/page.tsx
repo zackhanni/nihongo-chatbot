b6bfdfff152239aac76842963c0bda5444cc5530
Zack Hanni <zack.hannI@gmail.com>
2024-06-10 22:21:38 -0400

    set up api call to openai website.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import { useState } from "react";
 
+// const api_key = process.env.API_KEY;
+const api_key = "";
+
 const demoMessages = [
   [
     "ai",
@@ -15,21 +18,79 @@ const demoMessages = [
 
 export default function Home() {
   const [chats, setChats] = useState(demoMessages);
+  const [messages, setMessages] = useState([
+    {
+      message:
+        "Hello and thank you for visiting Nihongo AI Chat! Please type below to begin the conversation.",
+      sender: "ChatGPT",
+    },
+  ]);
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(api_key);
     e.preventDefault();
     setIsLoading(true);
     setChats([...chats, ["user", inputValue]]);
-    setInputValue("");
-    try {
-      // connect to chatGPT and get a response
-      // setIsLoading(false);
-    } catch (error) {
-      console.log("Error", error);
-    }
+    setInputValue(""); // clear input field
+
+    const newMessage = {
+      message: inputValue,
+      sender: "user",
+    };
+
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+
+    await processMessageToChatGPT(newMessages);
   };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content:
+        "You are a helpful AI designed to assist users in practicing their Japanese language skills. Engage in a conversation with short, 1 to 2 sentence responses in Japanese. Make sure to correct any mistakes politely and provide the correct way to say things if necessary. Keep the tone friendly and encouraging.",
+    };
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + api_key,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+          },
+        ]);
+        setIsLoading(false);
+      });
+  }
 
   return (
     <main className="flex items-center justify-center h-screen w-screen bg-gray-300">
@@ -39,29 +100,7 @@ export default function Home() {
             <p className="fw-bold text-white mb-0 py-2">Live AI chat</p>
           </div>
           <div className="card-body min-h-[50svh] max-h-[80svh] overflow-y-auto">
-            {/* default AI chat bubble style */}
-            {/* <div className="d-flex flex-row justify-content-start mb-4">
-              <p className="bg-red-500 size-10 px-3 rounded-full text-white items-center justify-center flex text-bold text-xl">
-                AI
-              </p>
-              <div className="p-3 ms-3 rounded-2xl bg-[#39c0ed33]">
-                <p className="small mb-0">{chats[0][1]}</p>
-              </div>
-            </div> */}
-
-            {/* default user chat bubble style */}
-            {/* <div className="d-flex flex-row justify-content-end mb-4">
-              <div className="p-3 me-3 border rounded-2xl bg-[#fbfbfb;]">
-                <p className="small mb-0">
-                  Thank you, I really like your product.
-                </p>
-              </div>
-              <p className="bg-blue-500 size-10 px-3 rounded-full text-white items-center justify-center flex text-bold text-xl">
-                You
-              </p>
-            </div> */}
-            {/* chat from database starts here */}
-            {chats.map((chat) => {
+            {/* {chats.map((chat) => {
               if (chat[0] === "ai") {
                 return (
                   <div
@@ -84,6 +123,38 @@ export default function Home() {
                   >
                     <div className="p-3 me-3 border rounded-2xl bg-[#fbfbfb;]">
                       <p className="small mb-0">{chat[1]}</p>
+                    </div>
+                    <p className="bg-blue-500 size-10 px-3 rounded-full text-white items-center justify-center flex text-bold text-xl">
+                      You
+                    </p>
+                  </div>
+                );
+              }
+            })} */}
+
+            {messages.map((message) => {
+              if (message.sender === "ChatGPT") {
+                return (
+                  <div
+                    key={message.message}
+                    className="d-flex flex-row justify-content-start mb-4"
+                  >
+                    <p className="bg-red-500 size-10 px-3 rounded-full text-white items-center justify-center flex text-bold text-xl">
+                      AI
+                    </p>
+                    <div className="p-3 ms-3 rounded-2xl bg-[#39c0ed33]">
+                      <p className="small mb-0">{message.message}</p>
+                    </div>
+                  </div>
+                );
+              } else {
+                return (
+                  <div
+                    key={message.message}
+                    className="d-flex flex-row justify-content-end mb-4"
+                  >
+                    <div className="p-3 me-3 border rounded-2xl bg-[#fbfbfb;]">
+                      <p className="small mb-0">{message.message}</p>
                     </div>
                     <p className="bg-blue-500 size-10 px-3 rounded-full text-white items-center justify-center flex text-bold text-xl">
                       You
